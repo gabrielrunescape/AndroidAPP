15ea7f80339f1bc9da1f04d7e318e02f19e9dae3
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-10-19 22:36:05 -0200

    Definição do layout do arquivo activity_login.xml

--- app/src/main/java/androidapp/gabrielrunescape/com/br/LoginActivity.java
+++ app/src/main/java/androidapp/gabrielrunescape/com/br/LoginActivity.java
@@ -1,13 +1,16 @@
 package androidapp.gabrielrunescape.com.br;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_login);
     }
 }
