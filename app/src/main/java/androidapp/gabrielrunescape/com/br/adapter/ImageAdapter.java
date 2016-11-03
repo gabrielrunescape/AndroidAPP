15e3d4584719f86baae0f856592a72f4b1080cf0
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-11-03 01:14:09 -0200

    Melhorias no layout pós login, adicionado item_main.xml e melhorias no ImageAdapter.java

--- app/src/main/java/androidapp/gabrielrunescape/com/br/adapter/ImageAdapter.java
+++ app/src/main/java/androidapp/gabrielrunescape/com/br/adapter/ImageAdapter.java
@@ -1,6 +1,6 @@
 package androidapp.gabrielrunescape.com.br.adapter;
 
-import android.animation.ObjectAnimator;
+import java.util.*;
 import android.view.*;
 import android.widget.*;
 import android.content.Context;
@@ -9,7 +9,8 @@ import androidapp.gabrielrunescape.com.br.R;
 /**
  *      Criado por GabrielRuneScape <gabrielfilipe@mail.ru> em 02/11/2016.
  *
- *      Classe adaptadora responsável pelo activity_main.xml.
+ *      Classe adaptadora responsável pelo activity_main.xml e por item_main.xml em relação ao seu
+ * design e layout.
  *
  *      Arquivo original obtivo em:
  *      https://developer.android.com/guide/topics/ui/layout/gridview.html, acesso em 2 nov de 2016.
@@ -17,13 +18,24 @@ import androidapp.gabrielrunescape.com.br.R;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    List<Choose> itens = new ArrayList<>();
 
     public ImageAdapter(Context c) {
         mContext = c;
+
+        itens.add(new Choose(R.drawable.sample_2, "Delire"));
+        itens.add(new Choose(R.drawable.sample_3, "Dans mon"));
+        itens.add(new Choose(R.drawable.sample_4, "Option"));
+        itens.add(new Choose(R.drawable.sample_5, "Deconnectés"));
+        itens.add(new Choose(R.drawable.sample_6, "DJ Snakey"));
+        itens.add(new Choose(R.drawable.sample_7, "Android Visual"));
+        itens.add(new Choose(R.drawable.sample_8, "Y no que más na"));
+        itens.add(new Choose(R.drawable.sample_1, "Cactesas"));
+        itens.add(new Choose(R.drawable.sample_3, "Delire"));
     }
 
     public int getCount() {
-        return mThumbIds.length;
+        return itens.size();
     }
 
     public Object getItem(int position) {
@@ -36,35 +48,38 @@ public class ImageAdapter extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-
         if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(160, 160));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.item_main, null);
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        final ImageView image = (ImageView) convertView.findViewById(R.id.imageChoose);
+        final TextView label = (TextView) convertView.findViewById(R.id.labelChoose);
+
+        convertView.setPadding(14, 14, 14, 14);
+
+        image.setImageResource(itens.get(position).getIcon());
+        label.setText(itens.get(position).getDesc());
+
+        return convertView;
     }
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_8, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_8, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
-    };
+}
 
+class Choose {
+    private Integer icon;
+    private String desc;
+
+    public Choose(Integer icon, String desc) {
+        this.desc = desc;
+        this.icon = icon;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public Integer getIcon() {
+        return icon;
+    }
 }
