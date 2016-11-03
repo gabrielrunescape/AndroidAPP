04dce45c92b737665c71b6926ff62ca81e5d2cba
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-11-02 23:54:27 -0200

    Plano de trabalho para novo layout em activity_main.xml

---
+++ app/src/main/java/androidapp/gabrielrunescape/com/br/adapter/ImageAdapter.java
@@ -0,0 +1,70 @@
+package androidapp.gabrielrunescape.com.br.adapter;
+
+import android.animation.ObjectAnimator;
+import android.view.*;
+import android.widget.*;
+import android.content.Context;
+import androidapp.gabrielrunescape.com.br.R;
+
+/**
+ *      Criado por GabrielRuneScape <gabrielfilipe@mail.ru> em 02/11/2016.
+ *
+ *      Classe adaptadora responsável pelo activity_main.xml.
+ *
+ *      Arquivo original obtivo em:
+ *      https://developer.android.com/guide/topics/ui/layout/gridview.html, acesso em 2 nov de 2016.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(160, 160));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_8, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_8, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+}
--- app/src/main/java/androidapp/gabrielrunescape/com/br/view/MainActivity.java
+++ app/src/main/java/androidapp/gabrielrunescape/com/br/view/MainActivity.java
@@ -3,16 +3,11 @@ package androidapp.gabrielrunescape.com.br.view;
 import android.view.*;
 import android.widget.*;
 import android.os.Bundle;
+import android.support.v7.app.*;
 import androidapp.gabrielrunescape.com.br.R;
-import androidapp.gabrielrunescape.com.br.controller.MainController;
-
-import android.support.v7.app.AppCompatActivity;
+import androidapp.gabrielrunescape.com.br.adapter.ImageAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvName;
-    private TextView tvEmail;
-    private Button btnLogout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        tvName = (TextView) findViewById(R.id.lbl_name);
-        tvEmail = (TextView) findViewById(R.id.lbl_email);
-        btnLogout = (Button) findViewById(R.id.btnLogout);
+        GridView gridview = (GridView) findViewById(R.id.mainGridView);
+        gridview.setAdapter(new ImageAdapter(this));
 
-        String name = this.getIntent().getExtras().getString("lbl_name");
-        String mail = this.getIntent().getExtras().getString("lbl_mail");
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        tvName.setText(name);
-        tvEmail.setText(mail);
-
-        btnLogout.setOnClickListener(new MainController(this, btnLogout, tvName, tvEmail));
     }
 }
